b9cf3e03d60fd912dfd5dac71e988aeeeea89601
Rifqy Pratama <60067877+Keinin04@users.noreply.github.com>
2021-01-31 19:50:27 +0700

    Episode

---
+++ Episode 04 - inisialisasi dan assignment nilai float/assignf.c
@@ -0,0 +1,17 @@
+/* Penulis : Rifqy, email rifqypratama04@gmail.com */
+
+#include<stdio.h>
+
+int main()
+{ /* Kamus */
+
+  float f = 0.555;
+  float x= 1.5E3;
+
+/* Algoritma */
+
+   printf ("Ini nilai f : %f \n", f);
+   printf ("Ini nilai f : %5.2f \n", f);
+   printf ("Ini nilai x : %10f \n", f);
+return 0;
+}
