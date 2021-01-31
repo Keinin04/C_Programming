10818493d2773a08b39225168ce0049ada5b7dbc
Rifqy Pratama <60067877+Keinin04@users.noreply.github.com>
2021-01-31 11:04:53 +0700

    Episode

---
+++ Episode 00 - halo/Halo_dunia.c
@@ -0,0 +1,13 @@
+/* File: halo.c /*
+/* Penulis : Rifqy pratama, rifqypratama04@gmail.com */
+/* Program menuliskan "Halo dunia" dan "end-of-line" (ganti baris) */
+
+#include <stdio.h>
+
+int
+main(void)
+{ /* Badan program */
+	printf("Halo dunia maya\n");
+	
+	
+}
---
+++ Episode 01 - program dengan kembalian nilai/nilai.c
@@ -0,0 +1,16 @@
+/* File: nilai.c
+   Penulis program: Rifqy, rifqypratama04@gmail.com
+   Program untuk menuliskan halo dunia ke layar */
+
+#include<stdio.h>
+
+int 
+main()
+{
+	/* KAMUS */
+
+	/* ALGORITMA */
+
+	printf("Halo dunia\n");
+	return 0;
+}
