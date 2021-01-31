9493961ac227a1bb705e261ec377e6d41e7413b0
Rifqy Pratama <60067877+Keinin04@users.noreply.github.com>
2021-01-31 19:05:21 +0700

    Episode

---
+++ Episode 02 - pengisian nilai dengan insialisasi atau variabel/initvar.c
@@ -0,0 +1,15 @@
+/* File : initvar.c */
+/* Penulis : Rifqy, rifqypratama04@gmail.com */
+/* Mengisi variabel i yg bertype in dengan nilai 5 dan menuliskannya */
+
+#include<stdio.h>
+
+int main ()
+{/* Kamus */
+  int i = 5; /* deklarasi dan inisialisasi nilai variabel i dengan 5 */
+  
+  /* Algoritma */
+  
+  printf ("Ini nilai i : %d \n", i); /*untuk mengamati dampak inisialisasi*/
+  return 0;
+}
---
+++ Episode 02 - pengisian nilai dengan insialisasi atau variabel/initvar_baru.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+
+	/* deklarasi */
+	int bebek = 10;
+	float luas = 2.5;
+	float keliling = 2*3.14*5.5;
+
+	printf("Total bebek: %d \n", bebek);
+	printf("Luas kebunku: %f  \n", luas);
+	printf("Keliling dari sebuah lingkaran yang jari jarinya: %f \n", keliling);
+
+	return 0;
+}
