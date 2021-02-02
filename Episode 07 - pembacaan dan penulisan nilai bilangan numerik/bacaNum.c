9837f35a4bf3bd9aeb61fb0e8ec063475d5afa9c
Rifqy Pratama <60067877+Keinin04@users.noreply.github.com>
2021-02-02 21:07:18 +0700

    Episode

---
+++ Episode 07 - pembacaan dan penulisan nilai bilangan numerik/bacaNum.c
@@ -0,0 +1,27 @@
+/* contoh membaca nilai numerik: bilangan bulat, bilangan riil */
+/* kemudian menuliskan nilai yang di baca */
+
+#include <stdio.h>
+int main ()
+{
+	/*kamus*/
+	int a;
+	float x;
+	
+
+	/*program*/
+	
+
+	printf("Ketik nilai integer: ");
+	scanf("%d", &a); /* membaca nilai a bertype integer, nama variable di tulis dengan &a(address of ...) */
+	printf("Nilai yang dibaca: %d \n", a);
+
+	printf("Ketik nilai bilangan riil: ");
+	scanf("%d", &x); 
+	printf("nilai yang dibaca: %f \n", x);
+
+
+
+
+	return 0;
+}
