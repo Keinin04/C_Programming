d550522306132d472eecbdb4532416619c532af4
Rifqy Pratama <60067877+Keinin04@users.noreply.github.com>
2021-02-08 12:12:24 +0700

    Episode

---
+++ Episode 08 - membaca nilai sebuah karakter/bacaChar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+	char rr;
+	char r;
+
+	printf("Baca 1 karakter saja: ");scanf("%c", &rr);
+	printf("%c \n", rr);
+	printf("Baca 1 karakter saja: ");scanf("%d", &r);
+	printf("%c \n", r);
+
+	return 0;
+}
